1a0ec6999f4eebcd1478343dd1733c64f20d8d7e
fourat53 <fourat610654@gmail.com>
2026-05-25 18:33:22 +0100

    fixed value issue

--- src/main/java/tn/esprit/back/config/RestConfig.java
+++ src/main/java/tn/esprit/back/config/RestConfig.java
@@ -1,14 +1,20 @@
 package tn.esprit.back.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import jakarta.persistence.metamodel.Type;
+import jakarta.persistence.EntityManager;
 
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
+  @Value("${front.dev.url}")
+  private String frontDevUrl;
+
+  @Value("${front.prod.url}")
+  private String frontProdUrl;
 
   private final EntityManager entityManager;
 
@@ -24,7 +30,7 @@ public class RestConfig implements RepositoryRestConfigurer {
     config.exposeIdsFor(classes);
 
     cors.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+        .allowedOrigins(frontDevUrl, frontProdUrl)
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
